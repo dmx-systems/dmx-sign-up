0379c60362e653a242b98c14de31f6fceaadfee3
Malte Reißig <malte@mikromedia.de>
2022-03-07 00:11:49 +0100

    Adapt to new ldap.changePassword impl. pt.7 (#34)

--- src/main/java/systems/dmx/signup/SignupPlugin.java
+++ src/main/java/systems/dmx/signup/SignupPlugin.java
@@ -295,7 +295,8 @@ public class SignupPlugin extends ThymeleafPlugin implements SignupPluginService
                 } else {
                     // LDAP requires plaintext password in credentials
                     String plaintextPassword = Base64.base64Decode(password);
-                    newCreds.password = plaintextPassword;
+                    newCreds.plaintextPassword = plaintextPassword;
+                    // newCreds.password = plaintextPassword;
                     ldapPluginService.changePassword(newCreds);
                 }
                 pwToken.remove(token);
