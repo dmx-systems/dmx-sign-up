4c53c1a72de892f320bbc5838e1c1d7200865d37
Malte Reißig <malte@mikromedia.de>
2022-03-30 00:54:35 +0200

    Minor UI/CSS form display adjustment in dark-bg style (#35)

--- src/main/java/systems/dmx/signup/SignupPlugin.java
+++ src/main/java/systems/dmx/signup/SignupPlugin.java
@@ -531,6 +531,7 @@ public class SignupPlugin extends ThymeleafPlugin implements SignupService, Post
     @Path("/custom-handle/{mailbox}/{displayname}/{password}")
     public Topic handleCustomAJAXSignupRequest(@PathParam("mailbox") String mailbox,
             @PathParam("displayname") String displayName, @PathParam("password") String password) throws URISyntaxException, WebApplicationException, RuntimeException {
+        // Double check: This may require "Administration" membership (or being at least authenticated, Systems WS, DisplayNames WS Id)
         Topic username = createCustomUserAccount(mailbox, displayName, password); // throws Exception if user account creation fails
         log.info("Created new user account for user with display \"" + displayName + "\" and mailbox " + mailbox);
         return username;
