ed59911410871971355d56048c04dbc1650182bf
Juergen Neumann <juergen@junes.eu>
2022-10-16 09:53:21 +0200

    test

---
+++ deploy/scripts/proxyport.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## calculates a random port number from a domain name
+## jpn - 20221015
+
+if [ -z "$1" ]; then
+    exit 1
+fi
+port="$( echo -n "$1" | sum | cut -d' ' -f1 )"
+port="$(( ${port} + 10000 ))"
+while [ ${port} -gt 65565 ]; do
+    port="$(( ${port} - 10000 ))"
+done
+echo "${port}"
