4b924be7ef6b6a3c126e3122709c2c20597a475d
Juergen Neumann <juergen@junes.eu>
2023-12-21 16:16:56 +0100

    debug login failure

--- deploy/tests/10_sign-up-create-ldap-users.sh
+++ deploy/tests/10_sign-up-create-ldap-users.sh
@@ -21,11 +21,10 @@ URL='core/topic/uri/dmx.workspaces.administration'
 BASE64="$( echo -n "${USERNAME}:${PASSWORD}" | base64 )"
 AUTH="Authorization: Basic ${BASE64}"
 #SESSIONID="$( curl -sS -H "${AUTH}" "${HOST}/${URL}" -i 2>&1 | grep ^Set-Cookie: | cut -d';' -f1 | cut -d'=' -f2 )"
-#echo "curl -sS -H "${AUTH}" "${HOST}/${URL}" -i 2>&1"
+echo "curl -sS -H "${AUTH}" "${HOST}/${URL}" -i 2>&1"
 SESSION="$( curl -sS -H "${AUTH}" "${HOST}/${URL}" -i 2>&1 )"
 #echo "SESSION = ${SESSION}"
 HTTPCODE="$( echo "${SESSION}" | grep HTTP | cut -d' ' -f2 )"
-LDAPPASSWORD='testpass'
 
 if [ "${HTTPCODE}" != "200" -a "${HTTPCODE}" != "204" ]; then
     echo "login ${USERNAME} failed! (HTTPCODE=${HTTPCODE})"
