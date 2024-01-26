5cd270931378e880764fadfbab1e90461dc5362f
Robert <robert.schuster.rs01@gmail.com>
2024-01-26 14:07:27 +0100

    Fix displayname_is_username constant

--- src/main/java/systems/dmx/signup/configuration/UsernamePolicy.java
+++ src/main/java/systems/dmx/signup/configuration/UsernamePolicy.java
@@ -1,7 +1,7 @@
 package systems.dmx.signup.configuration;
 
 public enum UsernamePolicy {
-    UNCONFINED, DISPLAYNAME_IS_USERNAME_, USERNAME_IS_EMAIL;
+    UNCONFINED, DISPLAYNAME_IS_USERNAME, USERNAME_IS_EMAIL;
 
     static UsernamePolicy fromStringOrAgnostic(String value) {
         try {
