5e8fab92ee8b5b5bcd35c012770aaefd061eafe0
Juergen Neumann <juergen@junes.eu>
2024-03-02 12:41:50 +0100

    CI: check job id.

--- deploy/ci-get-job-id.sh
+++ deploy/ci-get-job-id.sh
@@ -2,6 +2,8 @@
 echo "curl -sS --header \"JOB_TOKEN: ${CI_JOB_TOKEN}\" \"https://git.dmx.systems/api/v4/projects/${CI_PROJECT_ID}/pipelines/${CI_PIPELINE_ID}/jobs?scope[]=success\""
 RESULT="$( curl -sS --header "JOB_TOKEN: ${CI_JOB_TOKEN}" "https://git.dmx.systems/api/v4/projects/${CI_PROJECT_ID}/pipelines/${CI_PIPELINE_ID}/jobs?scope[]=success" )"
 MAVEN_BUILD_JOB_ID="$( echo "${RESULT}" | jq -c '.[] | select(.name | contains("maven-build")).id' )"
-echo "MAVEN_BUILD_JOB_ID=${MAVEN_BUILD_JOB_ID}"
-RESULT="$( curl -sS --header "JOB_TOKEN: ${CI_JOB_TOKEN}" "https://git.dmx.systems/api/v4/projects/${CI_PROJECT_ID}/jobs/${MAVEN_BUILD_JOB_ID}" )"
-echo "${RESULT}"
+echo -n "MAVEN_BUILD_JOB_ID=${MAVEN_BUILD_JOB_ID}"
+[ ! -z "${MAVEN_BUILD_JOB_ID##*[!0-9]*}" ] && echo " is a number" || echo " is not a number";
+
+#RESULT="$( curl -sS --header "JOB_TOKEN: ${CI_JOB_TOKEN}" "https://git.dmx.systems/api/v4/projects/${CI_PROJECT_ID}/jobs/${MAVEN_BUILD_JOB_ID}" )"
+#echo "${RESULT}"
