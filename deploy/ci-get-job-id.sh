7a3cfb59d4e87a201bf741c2ac558ac79b7bf984
Juergen Neumann <juergen@junes.eu>
2024-03-02 12:11:44 +0100

    CI: use tagged runners.

--- deploy/ci-get-job-id.sh
+++ deploy/ci-get-job-id.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "curl -sS --header \"JOB_TOKEN: ${CI_JOB_TOKEN}\" \"https://git.dmx.systems/api/v4/projects/${CI_PROJECT_ID}/pipelines/${CI_PIPELINE_ID}/jobs?scope[]=success\""
 RESULT="$( curl -sS --header "JOB_TOKEN: ${CI_JOB_TOKEN}" "https://git.dmx.systems/api/v4/projects/${CI_PROJECT_ID}/pipelines/${CI_PIPELINE_ID}/jobs?scope[]=success" )"
-echo "${RESULT}" | jq -c '.[] | select(.name | contains("maven-build"))'
+echo "${RESULT}" | jq -c '.[] | select(.name | contains("maven-build")).id'
 
