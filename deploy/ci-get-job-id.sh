1a2a02288536f51a449d9aaa0849938fc8dbb8ce
Juergen Neumann <juergen@junes.eu>
2024-03-02 16:14:36 +0100

    CI: test new ci-publish script.

--- deploy/ci-get-job-id.sh
+++ deploy/ci-get-job-id.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-echo "curl -sS --header \"JOB_TOKEN: ${CI_JOB_TOKEN}\" \"https://git.dmx.systems/api/v4/projects/${CI_PROJECT_ID}/pipelines/${CI_PIPELINE_ID}/jobs?scope[]=success\""
+
 RESULT="$( curl -sS --header "JOB_TOKEN: ${CI_JOB_TOKEN}" "https://git.dmx.systems/api/v4/projects/${CI_PROJECT_ID}/pipelines/${CI_PIPELINE_ID}/jobs?scope[]=success" )"
 MAVEN_BUILD_JOB_ID="$( echo "${RESULT}" | jq -c '.[] | select(.name | contains("maven-build")).id' )"
-echo -n "MAVEN_BUILD_JOB_ID=${MAVEN_BUILD_JOB_ID}"
-[ ! -z "${MAVEN_BUILD_JOB_ID##*[!0-9]*}" ] && echo " is a number" || echo " is not a number";
-
-#RESULT="$( curl -sS --header "JOB_TOKEN: ${CI_JOB_TOKEN}" "https://git.dmx.systems/api/v4/projects/${CI_PROJECT_ID}/jobs/${MAVEN_BUILD_JOB_ID}" )"
-#echo "${RESULT}"
+if [ ! -z "${MAVEN_BUILD_JOB_ID##*[!0-9]*}" ]; then
+    echo "INFO: Found job id from maven build job. (MAVEN_BUILD_JOB_ID=${MAVEN_BUILD_JOB_ID})"
+else
+    echo "ERROR! Could not get job id from maven build job."
+    exit 1
+fi
